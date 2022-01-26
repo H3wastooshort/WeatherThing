8f155c9262c7b88f763e1f84d417b9ecbdf4a950
HACKER3000 <hacker3000@posteo.org>
2022-01-26 21:08:23 +0100

    No more accidental resetting.
    Now you have to push a button

--- voltage reporting/reset.php
+++ voltage reporting/reset.php
@@ -1,11 +1,26 @@
 <?php
-header("Content-Type: text/plain");
-$filename = "data.csv";
-$content = "unixtime,voltage\n";
-$backupname = "data-backup-" . time() . ".csv";
-copy($filename, $backupname);
-$file = fopen($filename,"w");
-fwrite($file, $content);
-fclose($file);
-print("Data Reset.");
+if (isset($_POST["reset"])) {
+	header("Content-Type: text/plain");
+	$filename = "data.csv";
+	$content = "unixtime,voltage\n";
+	$backupname = "data-backup-" . time() . ".csv";
+	copy($filename, $backupname);
+	$file = fopen($filename,"w");
+	fwrite($file, $content);
+	fclose($file);
+	die("Data Reset.");
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Solar Voltage History Reset</title>
+</head>
+<body>
+<h1>Solar Voltage History Reset</h1>
+<form method="POST" formaction="reset.php">
+<input type="submit" name="reset" value="Reset History">
+</form>
+</body>
+</html>
